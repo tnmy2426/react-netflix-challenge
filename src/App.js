ee91f07568ddf05ef705f56e38c0d84d06fc113e
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-18 23:28:12 +0600

    Netflix project challenge Part-One Completed without css styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!</h1>
+      <Card img_src="#" img_alt="DarkPic" card_category="A netflix Original Film" card_title="DARK" link="https://www.google.com" />
+      <Card img_src="#" img_alt="TheFlash" card_category="A netflix Original Series" card_title="FLASH" link="https://www.facebook.com" />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='card__container'>
             <div className='cards'>
                 <div className='card'>
-                    <img src="#" alt="Dot" className='card__image' />
+                    <img src={props.img_src} alt={props.img_alt} className='card__image' />
                     <div className='card__info'>
-                        <span className='card__category'>A netflix Original Film</span>
-                        <h3 className='card__title'>DARK</h3>
-                        <a href="#" target="_blank">
+                        <span className='card__category'>{props.card_category}</span>
+                        <h3 className='card__title'>{props.card_title}</h3>
+                        <a href={props.link} target="_blank">
                             <button>Watch Now</button>
                         </a>
                     </div>
